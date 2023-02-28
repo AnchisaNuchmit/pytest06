910e0a9d0a364ea025af747565482fc2f60f4086
Anchisatk <117658112+AnchisaNuchmit@users.noreply.github.com>
2023-02-28 13:39:09 +0700

    Add files via upload

---
+++ function.py
@@ -0,0 +1,27 @@
+def validate_number(number):
+    if type(number) != int:
+        if type(number) == str:
+            return "Input integer"
+        elif number >= 1 and number <= 12:
+            return "Input integer"
+        else:
+            return "out of range"
+    elif number >= 1 and number <= 12:
+        return number
+    else:
+            return "out of range"
+def number_to_month(number):
+    month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    return month[number-1]
+
+
+def display_month(number):
+    result = validate_number(number)
+    if type(result) == int:
+        return number_to_month(result)
+    else:
+        return result
+
+    
+
+
